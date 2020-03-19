67c21aa03b492de5297ad5c5e74d2ea49aa7ac9d
Stephen Cases <builtforsomethingbigger@gmail.com>
2020-03-19 02:29:57 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -18,7 +18,4 @@ def all_phrases(catch_phrases)
   phrase = "It's-a me, Mario!"
   status = "Thank You Mario! But Our Princess Is In Another Castle!"
   Phrase = "It's Dangerous To Go Alone! Take This."
-  puts phrase
-  puts status
-  puts Phrase
 end
